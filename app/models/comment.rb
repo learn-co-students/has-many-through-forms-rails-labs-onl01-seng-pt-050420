4104dc24cee323698cc0b9f7ca4a8d99fde14e9c
C0mbed <matt@farncombe.co>
2020-09-02 12:55:27 -0600

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,17 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  accepts_nested_attributes_for :user, reject_if: :user_blank?
+  accepts_nested_attributes_for :user
 
-  def user_blank?
-   byebug
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      if user_attribute != ""
+        user = User.find_or_create_by(username: user_attribute)
+        self.user = user
+      end
+    end
   end
+
+
+
 end
