93b35e0a033a5e7f6323fd613442ba8494226c34
C0mbed <matt@farncombe.co>
2020-09-02 10:47:44 -0600

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: :user_blank?
+
+  def user_blank?
+   byebug
+  end
 end
