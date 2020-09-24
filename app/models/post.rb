c240adb811f86ad038567cab22d58f3701853678
morgancollado <Morgan.collado@gmail.com>
2020-09-23 19:49:37 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,16 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
+
+  def category_attributes=(category_attributes)
+    category_attributes.values.each do |c_a|
+      if c_a[:name].present?
+        category = Category.find_or_create_by(c_a)
+        self.categories << category
+      end 
+    end  
+  end 
 
 
 end
